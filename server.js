51f518fb530249a1462c09b24956cc3295da8eec
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-21 23:41:59 -0400

    added a server for heroku deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/christian-gracia-site"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/christian-gracia-site/index.html"));
+});
+
+app.listen(process.env.PORT || 8080);
