70c5818185753d8abb2523cf7ba2a1659b46dc1f
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-06 23:06:38 -0400

    added github repo api call with some config vars set up for local staging and prod

--- src/app/screens/projects-page/github-repo/github-repo.component.ts
+++ src/app/screens/projects-page/github-repo/github-repo.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GithubService } from "../../../services/github.service";
 
 @Component({
-  selector: 'app-github-repo',
-  templateUrl: './github-repo.component.html',
-  styleUrls: ['./github-repo.component.scss']
+  selector: "app-github-repo",
+  templateUrl: "./github-repo.component.html",
+  styleUrls: ["./github-repo.component.scss"],
 })
 export class GithubRepoComponent implements OnInit {
-
-  constructor() { }
+  constructor(private githubService: GithubService) {}
 
   ngOnInit() {
+    console.log(this.githubService.sayHi());
   }
-
 }
