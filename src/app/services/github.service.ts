4016b34d248a9d9c1d4c15afd058f5edeced3ce1
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-11 02:07:21 -0400

    another fix for error on deploy

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../environments/environment";
+import { map } from "rxjs/operators";
 import { Repo } from "../models";
 
 @Injectable({
@@ -10,8 +11,10 @@ export class GithubService {
   constructor(private http: HttpClient) {}
 
   public getAllRepos(): Repo[] {
-    return this.http
-      .get(environment.apiUrl + "/github/all-repos")
-      .pipe((data: Repo[]) => data);
+    return this.http.get(environment.apiUrl + "/github/all-repos").pipe(
+      map((data: Repo[]) => {
+        return data;
+      })
+    );
   }
 }
