4b17d030ddefa78e7ea74c858c4e7010d89d287b
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-24 00:48:54 -0400

    added location data to contact form

--- src/app/screens/contact-page/contact-form/contact-form.component.ts
+++ src/app/screens/contact-page/contact-form/contact-form.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from "@angular/forms";
 import { EmailService } from "../../../services/email.service";
 import { EmailMessage } from "src/app/models/email-message.model";
 import { RoutingService } from "src/app/services/routing.service";
+import { LocationData } from "src/app/models/location-data.model";
+import { LocationService } from "../../../services/location.service";
 
 @Component({
   selector: "app-contact-form",
@@ -12,7 +14,8 @@ import { RoutingService } from "src/app/services/routing.service";
 export class ContactFormComponent implements OnInit {
   constructor(
     private emailService: EmailService,
-    private routingService: RoutingService
+    private routingService: RoutingService,
+    private locationService: LocationService
   ) {}
 
   emailReceived: boolean = false;
@@ -25,6 +28,11 @@ export class ContactFormComponent implements OnInit {
       form.value.email,
       form.value.message
     );
+    this.locationService
+      .getLocationJSON()
+      .subscribe((locationData: LocationData) => {
+        this.emailService.sendSiteVisitEmail(locationData).subscribe(() => {});
+      });
     this.emailService.sendContactEmail(messageParams).subscribe((data: any) => {
       if (data.name) {
         this.emailReceived = true;
