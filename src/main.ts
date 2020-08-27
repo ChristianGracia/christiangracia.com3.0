e3dd909ef1a7ec1aafec218e45249ce60848106c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-26 21:56:20 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode } from "@angular/core";
+import { enableProdMode, isDevMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
-if (environment.production || process.env.MONGO) {
+if (environment.production || !isDevMode()) {
   enableProdMode();
 }
 
