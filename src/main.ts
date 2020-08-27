73bc3120742b09d519688acf43a9dc7c1600d96d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-26 22:05:31 -0400

    trying this build script

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode, isDevMode } from "@angular/core";
+import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
-if (environment.production || !isDevMode()) {
+if (environment.production) {
   enableProdMode();
 }
 
