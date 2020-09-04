3e9576bd09fef64995345566c2cd590adac00600
Christian Gracia <christiangracia.student@careerdevs.com>
2020-09-04 00:25:37 -0400

    added a scroll to top event on page change

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -10,7 +10,6 @@ export class ProjectsComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {}
   ngAfterViewInit() {
-    // Hack: Scrolls to top of Page after page view initialized
     let top = document.getElementById("top");
     if (top !== null) {
       top.scrollIntoView();
