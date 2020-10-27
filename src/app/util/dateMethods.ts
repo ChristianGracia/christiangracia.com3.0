bc4c7c67e16afa6c5f7b0705da2c6b1ec36b8975
Christian Gracia <christiangracia.student@careerdevs.com>
2020-10-26 23:51:19 -0400

    added unicode symbols for day or night time displays

--- src/app/util/dateMethods.ts
+++ src/app/util/dateMethods.ts
@@ -1,6 +1,7 @@
 function militaryTimeConverter(date: Date) {
   let hours = date.getHours();
   const AmOrPm = hours >= 12 ? "PM" : "AM";
+
   hours = hours % 12 || 12;
   let minutes = date.getMinutes();
   let formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
@@ -9,9 +10,13 @@ function militaryTimeConverter(date: Date) {
 }
 function formatDateAndTime(date: string) {
   let formattedDate = new Date(date);
+  let hours = formattedDate.getHours();
 
+  const dayOrNight = hours > 19 || hours < 5 ? "🌙" : "🌞";
   return (
     militaryTimeConverter(formattedDate) +
+    " " +
+    dayOrNight +
     " " +
     formattedDate.toLocaleDateString("en-US")
   );
