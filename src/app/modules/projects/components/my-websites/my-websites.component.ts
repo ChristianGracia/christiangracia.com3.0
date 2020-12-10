3392721f1b80236e2c6457dcb7df1ff629e3deb5
Christian Gracia <christiangracia.student@careerdevs.com>
2020-12-10 00:18:15 -0500

    updated sites text content

--- src/app/modules/projects/components/my-websites/my-websites.component.ts
+++ src/app/modules/projects/components/my-websites/my-websites.component.ts
@@ -4,7 +4,7 @@ import { Site } from "../../models/site.model";
 const CHRISTIAN_GRACIA_SITE: Site = new Site(
   "christiangracia.com v2",
   `My previous site that I wrote in TypeScript with only stateless functional components and React Hooks.
-  React/Redux + Sass front-end. This is hosted on a free dyno so it takes 2 mins to wake up`,
+  React/Redux + Sass front-end.`,
   "https://christiangracia-old.herokuapp.com/",
   [
     "https://i.imgur.com/dga9J4G.jpg",
@@ -15,9 +15,8 @@ const CHRISTIAN_GRACIA_SITE: Site = new Site(
 
 const ON_A_BEAT_SITE: Site = new Site(
   "On a Beat!",
-  `MERN stack site I wrote and host for my friends in On a Beat. Hosted on Heroku on a single server.
-  React/Redux front-end with a Node Js back-end and a MongoDB database. Users can make accounts, sign in,
-  add/delete videos, songs, pics, and gifs to front page, and more with a site dashboard I built. Currently hosted on a free heroku dyno after I recently took the site down so expect it to take 1-2 mins to wake up`,
+  `A MERN stack site I wrote while learning to code, React/Redux front-end with a Node Js back-end and a MongoDB database. Users can make accounts, sign in,
+  add/delete videos, songs, pics, and gifs to front page, and more with a site dashboard I built`,
   "https://onabeat.herokuapp.com/",
   [
     "https://i.imgur.com/h9Bm4yj.jpeg",
@@ -29,7 +28,7 @@ const ON_A_BEAT_SITE: Site = new Site(
 
 const NFL_SITE: Site = new Site(
   "Nature's Frontier Landscaping",
-  "React front-end + NodeJS back-end site I made for Nature's Frontier Landscaping. Live at nflandscaping.com",
+  "React front-end + NodeJS back-end site I made for Nature's Frontier Landscaping. Live at nflandscaping.com. Will be rewritten soon, I made this while just learning React",
   "https://www.nflandscaping.com",
   [
     "https://i.imgur.com/TTgeW7Y.jpeg",
