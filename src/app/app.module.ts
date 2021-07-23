fc5b2eb1eefbe1653ae108f413b09184707da00e
Christian Gracia <christiangracia.student@careerdevs.com>
2021-07-22 21:56:47 -0500

    more refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MaterialModule } from "./material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomePageComponent } from "./screens/home-page/home-page.component";
-
 import { HeaderComponent } from "./navigation/header/header.component";
-
 import { SideNavigationBarComponent } from "./navigation/side-navigation-bar/side-navigation-bar.component";
 import { PersonalInfoComponent } from "./screens/home-page/personal-info/personal-info.component";
-
 import { SocialMediaLinksComponent } from "./screens/home-page/social-media-links/social-media-links.component";
-
 import { FooterComponent } from "./components/footer/footer.component";
-import { HttpClientModule } from "@angular/common/http";
-
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [
