a7321940f832fb21bfd50646fcb416fccdceaf6c
Christian Gracia <christiangracia.student@careerdevs.com>
2021-07-27 00:58:50 -0500

    conversion almost done

--- src/app/modules/projects/modals/repo-commit-modal/repo-commit-modal.component.ts
+++ src/app/modules/projects/modals/repo-commit-modal/repo-commit-modal.component.ts
@@ -21,7 +21,7 @@ export interface DialogData {
   templateUrl: "./repo-commit-modal.component.html",
   styleUrls: ["./repo-commit-modal.component.scss"],
 })
-export class RepoCommitModalComponent implements OnInit, AfterViewInit {
+export class RepoCommitModalComponent implements OnInit {
   public loadingCommits: boolean = false;
   public commits: Commit[] = [];
   public displayedCommits: Commit[] = [];
@@ -31,38 +31,15 @@ export class RepoCommitModalComponent implements OnInit, AfterViewInit {
   public page = 0;
   public size = 25;
 
-  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
+  displayedColumns: string[] = ["message", "time", "url"];
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
+  // ngAfterViewInit() {
+  //   this.dataSource.paginator = this.paginator;
+  // }
 
-  ELEMENT_DATA = [
-    { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-    { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-    { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-    { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-    { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-    { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-    { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-    { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-    { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-    { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-    { position: 11, name: "Sodium", weight: 22.9897, symbol: "Na" },
-    { position: 12, name: "Magnesium", weight: 24.305, symbol: "Mg" },
-    { position: 13, name: "Aluminum", weight: 26.9815, symbol: "Al" },
-    { position: 14, name: "Silicon", weight: 28.0855, symbol: "Si" },
-    { position: 15, name: "Phosphorus", weight: 30.9738, symbol: "P" },
-    { position: 16, name: "Sulfur", weight: 32.065, symbol: "S" },
-    { position: 17, name: "Chlorine", weight: 35.453, symbol: "Cl" },
-    { position: 18, name: "Argon", weight: 39.948, symbol: "Ar" },
-    { position: 19, name: "Potassium", weight: 39.0983, symbol: "K" },
-    { position: 20, name: "Calcium", weight: 40.078, symbol: "Ca" },
-  ];
-
-  dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
+  dataSource;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private githubService: GithubService
@@ -90,6 +67,14 @@ export class RepoCommitModalComponent implements OnInit, AfterViewInit {
     this.githubService
       .getAllCommitsOfRepo(this.data.repo, this.numberOfCommits)
       .subscribe((data) => {
+        this.dataSource = new MatTableDataSource<any>(
+          data.map((item) => ({
+            message: item.message,
+            time: item.time,
+            url: item.url,
+          }))
+        );
+        this.dataSource.paginator = this.paginator;
         this.commits = data;
         this.loadingCommits = false;
         this.totalCommits = data.length;
