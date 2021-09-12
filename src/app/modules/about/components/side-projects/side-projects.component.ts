30e0e02f0707348707b006e09abd8da74c0d0378
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-12 17:06:59 -0500

    moved site class to shared module, more side project setup

--- src/app/modules/about/components/side-projects/side-projects.component.ts
+++ src/app/modules/about/components/side-projects/side-projects.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Site } from "../../../shared-components/models/site.model";
 
+const NFL_SITE: Site = new Site(
+  "Nature's Frontier Landscaping",
+  "React front-end + NodeJS back-end site I made for Nature's Frontier Landscaping. Live at nflandscaping.com. Will be rewritten soon, I made this while just learning React",
+  "https://www.nflandscaping.com",
+  [
+    "https://i.imgur.com/TTgeW7Y.jpeg",
+    "https://i.imgur.com/dufsNaC.png",
+    "https://i.imgur.com/TIkU3jl.png",
+  ]
+);
+
+const MARKETFEELS: Site = new Site(
+  "MarketFeels",
+  "Server side rendered React + Redux finance site written in TypeScript",
+  "https://marketfeels.com",
+  ["https://i.imgur.com/D1ualvH.jpeg"]
+);
 @Component({
-  selector: 'app-side-projects',
-  templateUrl: './side-projects.component.html',
-  styleUrls: ['./side-projects.component.scss']
+  selector: "app-side-projects",
+  templateUrl: "./side-projects.component.html",
+  styleUrls: ["./side-projects.component.scss"],
 })
 export class SideProjectsComponent implements OnInit {
+  public sites: Site[] = [MARKETFEELS, NFL_SITE];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
