3a0c5cda234e70e9114e0e41ac69e8e761e93771
Christian Gracia <christiangracia.student@careerdevs.com>
2021-10-21 01:35:47 -0500

    added new site info

--- src/app/modules/about/components/side-projects/side-projects.component.ts
+++ src/app/modules/about/components/side-projects/side-projects.component.ts
@@ -12,13 +12,19 @@ const MARKETFEELS: Site = new Site(
   "Working on the ability to view finance info, post on social medias using bots (hype), add machine learning models, etc",
   "https://marketfeels.com"
 );
+
+const CG4: Site = new Site(
+  "Christiangracia.com4.0",
+  "My new site I am writing that will replace this one soon. Featuring Angular12, complete server sider rendering, minimal css, full use of Material themes, all around better",
+  "https://cg-playground.herokuapp.com/"
+);
 @Component({
   selector: "app-side-projects",
   templateUrl: "./side-projects.component.html",
   styleUrls: ["./side-projects.component.scss"],
 })
 export class SideProjectsComponent implements OnInit {
-  public sites: Site[] = [MARKETFEELS, NFL_SITE];
+  public sites: Site[] = [CG4, MARKETFEELS, NFL_SITE];
 
   constructor() {}
 
